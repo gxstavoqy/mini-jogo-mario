7e38ee1f3cd5eae1c081c8cefcd6f6b3472ddd8f
gxstavoqy <gustaavo.goncaalves@gmail.com>
2024-10-25 18:49:13 -0300

    atualizando informações do index e script

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = './projeto jogo do mario/images/game-over.png';
+        mario.src = './images/game-over.png';
         mario.style.width = '75px' 
         mario.style.marginLeft = '50px'
 
